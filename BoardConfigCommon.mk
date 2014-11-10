7e201c864d41c324afcd2bfb2e51b1f4be8f918b
Evisceration <eviscerationls@gmail.com>
2014-11-10 03:55:46 +0100

    msm8974-common: Do not mess with boot jars
    this line breaks building with 5.0
    
    see @ad6674cd6003b78ef8ba101eb8a6b3c889ae4b0d
    
    Change-Id: Ib59b21a9ceb6846780b61ced8ebbff981df66e37

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -93,8 +93,6 @@ BOARD_SEPOLICY_UNION += \
     genfs_contexts \
     te_macros
 
-PRODUCT_BOOT_JARS := $(subst $(space),:,$(PRODUCT_BOOT_JARS))
-
 # Graphics
 BOARD_EGL_CFG := device/oppo/msm8974-common/configs/egl.cfg
 USE_OPENGL_RENDERER := true
