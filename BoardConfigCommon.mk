d3df8ed52427523388c3f09a30ea9a50771dafc8
Brint E. Kriebel <bekit@cyngn.com>
2015-04-23 12:51:32 -0700

    msm8974: Disable dexpreopt for now
    Until compression is reenabled or another option to reduce space is
    found, disable dexpreopt.
    
    Change-Id: Ia8f9e3ccf6bd91911a139e5b3d7cf0779614088f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -148,13 +148,13 @@ EXTENDED_FONT_FOOTPRINT := true
 # TARGET_TRANSPARENT_COMPRESSION_METHOD := lz4
 
 # Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
+# ifeq ($(HOST_OS),linux)
+#   ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+#     ifeq ($(WITH_DEXPREOPT),)
+#       WITH_DEXPREOPT := true
+#     endif
+#   endif
+# endif
 
 # inherit from the proprietary version
 ifneq ($(QCPATH),)
