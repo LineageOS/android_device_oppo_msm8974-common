c35cfb5fd7b60a48719d1c7725e581b9657c5bb4
Sultanxda <sultanxda@gmail.com>
2015-07-14 02:47:28 -0700

    QCamera2: Reduce log spam
    Change-Id: Ie0900f054785932cc59c809d3b01e543c15f735b

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -19,8 +19,8 @@ LOCAL_SRC_FILES := \
     wrapper/QualcommCamera.cpp
 
 LOCAL_CFLAGS = -Wall -Werror
-#Debug logs are enabled
-#LOCAL_CFLAGS += -DDISABLE_DEBUG_LOG
+# Debug logs are disabled
+LOCAL_CFLAGS += -DDISABLE_DEBUG_LOG
 
 LOCAL_CFLAGS += -DDEFAULT_ZSL_MODE_ON
 LOCAL_CFLAGS += -DDEFAULT_DENOISE_MODE_ON
