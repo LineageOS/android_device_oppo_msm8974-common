aa25b0c8d9187c065715129c4ce7b3d83b5e20c8
khusika <khusikadhamar@gmail.com>
2015-08-10 19:55:51 +0700

    msm8974: Remove deprecated egl.cfg
    see here: https://github.com/CyanogenMod/android_frameworks_native/commit/993814255fc248454368ed9fe34b4703a05eaf99
    
    Change-Id: Ib29fc8685df1de424a0b50733acb0d194b8c9e4f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,7 +79,6 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BOARD_HARDWARE_CLASS += device/oppo/msm8974-common/cmhw
 
 # Graphics
-BOARD_EGL_CFG := device/oppo/msm8974-common/configs/egl.cfg
 USE_OPENGL_RENDERER := true
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
 TARGET_USES_C2D_COMPOSITION := true
