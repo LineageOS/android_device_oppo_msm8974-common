781b45e28dfd1ff8d75e0094a971bedf6fd3cd56
Brint E. Kriebel <bekit@cyngn.com>
2015-08-14 12:55:55 -0700

    dexpreopt: Only use boot dexpropt
    System space is limited on user builds. Only perform boot dexpreopt
    instead of full.
    
    Change-Id: I05537c02d159ca96ce0f063c8df09dc932c6026c
    Ticket: CYNGNOS-758
    (cherry picked from commit 56afe1ee7fda9540b8c164d6bd8b08ddef55cba9)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -152,8 +152,8 @@ EXTENDED_FONT_FOOTPRINT := true
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
+    ifeq ($(WITH_DEXPREOPT_BOOT_IMG_ONLY),)
+      WITH_DEXPREOPT_BOOT_IMG_ONLY := true
     endif
   endif
 endif
