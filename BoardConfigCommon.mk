a0e895778aaba0d07babca7bb025099a09b55acd
Michael Bestas <mikeioannina@gmail.com>
2015-08-22 18:25:33 +0300

    msm8974: Remove TARGET_USE_QCOM_BIONIC_OPTIMIZATION
    * Enabled by default now
    
    Change-Id: Ica80bd3343e8946caf6ae205b9155e4b9a52585a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,7 +35,6 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := krait
-#TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 # Assertions
 TARGET_BOARD_INFO_FILE ?= device/oppo/msm8974-common/board-info.txt
