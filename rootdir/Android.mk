46deaa395112e695c44f258e0ecb21758b684a48
Michael Bestas <mikeioannina@gmail.com>
2015-09-12 22:38:54 +0300

    msm8974: Cleanup after bacon split
    * Alphabetize
    * Remove stuff that depend on QCPATH
    * Fix setup-makefiles
    * Use oppo/msm8974 kernel
    
    Change-Id: I068b7707b0a0617ef11c0207d91f408f4fa63ce0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -43,10 +43,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.power.rc
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.cne.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.cne.rc
-include $(BUILD_PREBUILT)
