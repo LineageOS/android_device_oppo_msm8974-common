a925c8d07df168ad7a0565937d8911449b1d9544
Steve Kondik <steve@cyngn.com>
2015-10-19 14:43:11 -0700

    msm8974: Support for native tap-to-wake
    Change-Id: I25f300f91c512a784b2a6c8c2b43544b3ed4e4eb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,6 +72,7 @@ BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(PLATFORM_PATH)/charger/images
 # CM Hardware
 BOARD_USES_CYANOGEN_HARDWARE := true
 BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
