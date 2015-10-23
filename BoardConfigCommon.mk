e7bee8e99e2ee16c7123f8f84dc17cc030f9a4ab
Michael Bestas <mikeioannina@gmail.com>
2015-10-23 22:59:41 +0300

    msm8974: Disable local sepolicy for now
    Change-Id: If4afbb8b06742955ba58a66a06420759829cbce2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,7 +116,7 @@ TARGET_RIL_VARIANT := caf
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
