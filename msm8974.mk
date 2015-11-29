b8d09bf91c29dd283c2db89ac1e488ea25509b89
Michael Bestas <mikeioannina@gmail.com>
2015-11-29 20:33:24 +0200

    msm8974: Cleanup packages
    * Remove packages that are unused, already included in build or vendor
    
    Change-Id: I10fd66e537351cefcf64eb1508046b9496afa177

--- msm8974.mk
+++ msm8974.mk
@@ -84,11 +84,6 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     rmnetcli
 
-# Filesystem management tools
-PRODUCT_PACKAGES += \
-    make_ext4fs \
-    setup_fs
-
 # Graphics
 PRODUCT_PACKAGES += \
     copybit.msm8974 \
@@ -163,10 +158,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine-8974.conf
 
-# USB
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory
-
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
@@ -193,7 +184,6 @@ PRODUCT_PACKAGES += \
     ebtables \
     ethertypes \
     libnl_2 \
-    libbson \
     libcnefeatureconfig \
     libtinyxml \
     libxml2
