18d2a03de0e07e5805bf2ee14f9325d1dfbdeee3
martincz <martincz@mokeedev.com>
2016-01-08 12:27:46 +0800

    msm8974-common: verify device
    Change-Id: I79589316a4b0848b7dd6e5f58b45f539ed0f8dd7

--- dataservices/Android.mk
+++ dataservices/Android.mk
@@ -1,1 +1,9 @@
+ifeq ($(BOARD_VENDOR),oppo)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
+ifneq ($(filter find7 find7s n3,$(TARGET_DEVICE)),)
+
 include $(call all-subdir-makefiles)
+
+endif
+endif
+endif
