c449d31bbb9ffbf6f2242c97659218884d5b78ec
Steve Kondik <steve@cyngn.com>
2016-05-18 12:54:14 -0700

    msm8974: Reenable the QC location stack
     * Mystery solved, it works.
    
    This reverts commit 7cef4f344e5d09a9839a9e3d027a19089e6cd309.
    
    Change-Id: I6c33e07c517702331f05650dc85c84ded9781ec1

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -219,6 +219,7 @@ done
 
 PRODUCT_PACKAGES += \\
     CNEService \\
+    com.qualcomm.location \\
     dpmserviceapp \\
     shutdownlistener
 
@@ -292,6 +293,17 @@ include \$(CLEAR_VARS)
 LOCAL_MODULE := CNEService
 LOCAL_MODULE_OWNER := $VENDOR
 LOCAL_SRC_FILES := proprietary/priv-app/CNEService/CNEService.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := com.qualcomm.location
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
