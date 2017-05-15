eae0bf72a3b0115d2aedc99b33bf58376d90a331
Abhishek Arpure <aarpure@codeaurora.org>
2017-05-15 10:41:18 -0700

    msm8974-common: Move ueventd.qcom.rc to /vendor
    Change-Id: Iab2699bf26ca4402b835d1af8d4493f9b28595d9

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -47,8 +47,9 @@ LOCAL_MODULE       := ueventd.bacon.rc
 else
 LOCAL_MODULE       := ueventd.qcom.rc
 endif
+LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
