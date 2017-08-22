d08e179fb8e7e7663a05eb76d2db95fe062073ba
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:23:30 +0200

    oppo-8974: Add HIDL manifest
    Change-Id: I2c3aece8ad13613ec5b83496aa23d26ef238f45d

--- msm8974.mk
+++ msm8974.mk
@@ -92,6 +92,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
     $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
+
 # IPC router config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
