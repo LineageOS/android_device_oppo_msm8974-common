e60a3f7ddd51b16ecd7d7cbb296c966e8bcb3b40
Simon Shields <simon@lineageos.org>
2017-09-05 19:09:06 +1000

    msm8974: shims: add createSurface camera shim
    Change-Id: I013e66da4e7f58c0239776f637a2b512ece8d7b7

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -6,7 +6,11 @@ LOCAL_SRC_FILES := \
     camera_shim.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libui
+    libui \
+    libbase \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0 \
+    libgui
 
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
