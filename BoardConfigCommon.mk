df0cc8c295bbf2879414d15a93ab0b0cebbbe2f7
Ethan Chen <intervigil@gmail.com>
2017-09-26 08:43:10 -0700

    msm8974-common: Move wcnss_service to vendor
    Change-Id: I512a83aaa4dad8c2c613d08da1f4fbc2139514ea

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -131,6 +131,7 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn
 TARGET_USES_QCOM_WCNSS_QMI       := true
+PRODUCT_VENDOR_MOVE_ENABLED      := true
 TARGET_USES_WCNSS_MAC_ADDR_REV   := true
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
