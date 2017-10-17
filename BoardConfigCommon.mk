08191c52edf1c3a47ea9977676e47be274439a49
jrior001 <jriordan001@gmail.com>
2017-10-16 20:30:49 -0400

    msm8974: adapt cmhw to new lineagesdk structure
    Change-Id: Ie03c5664703ec3fd954efc71de47d6340086f79d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,7 +69,7 @@ BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(PLATFORM_PATH)/charger/images
 
 # CM Hardware
 BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
+BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/lineagehw
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Encryption
