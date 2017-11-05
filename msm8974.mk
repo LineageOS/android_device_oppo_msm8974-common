b3fe0a6e2b51105b705d67a9781140aa14d581f2
jrior001 <jriordan001@gmail.com>
2017-11-04 23:39:40 -0400

    msm8974: binderize memtrack
    Change-Id: I236caf6bec7287b6824f572d5a36f03a1f874ed6

--- msm8974.mk
+++ msm8974.mk
@@ -77,6 +77,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     copybit.msm8974 \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
