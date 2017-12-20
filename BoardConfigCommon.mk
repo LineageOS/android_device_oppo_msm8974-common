c26564ffe61f88765561e5eef8289cda683d5eea
Michael Bestas <mkbestas@lineageos.org>
2017-12-21 01:35:03 +0200

    msm8974: Include legacy qcom sepolicy rules
    Change-Id: Ic499de98c34a63de18e935d2cc6f23f0e69b1a43

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -118,6 +118,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
