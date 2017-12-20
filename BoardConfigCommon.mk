bf3b97b611478ca1bf2582c890a0f5fdfec231a2
Michael Bestas <mkbestas@lineageos.org>
2017-12-20 22:20:48 +0200

    msm8974: Enable pre-O hw crypto compat flag
    Change-Id: I0a3af6e4594a48c6364e999ca2b20a85a98035b3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,6 +78,7 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
+TARGET_LEGACY_HW_DISK_ENCRYPTION := true
 
 # Filesystem
 BOARD_FLASH_BLOCK_SIZE := 131072
