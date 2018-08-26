9de99ddc18296801d53932001595f661aaaa318a
jrior001 <jriordan001@gmail.com>
2018-08-26 18:47:33 -0400

    msm8974-common: Use common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- msm8974.mk
+++ msm8974.mk
@@ -207,6 +207,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/vendor/etc/thermal-engine-8974.conf
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
