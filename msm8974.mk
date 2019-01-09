5d6e9e6ec50ec84f33971197aff04976fbc8a10f
Joey <joey@lineageos.org>
2019-01-09 18:02:45 +0000

    msm8974-common: Build Trust HAL
    Change-Id: I2a426b2c2dda70f41e3c3ea6f85905b0daf65f0d
    Signed-off-by: Joey <joey@lineageos.org>

--- msm8974.mk
+++ msm8974.mk
@@ -221,6 +221,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oppo_msm8974
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
