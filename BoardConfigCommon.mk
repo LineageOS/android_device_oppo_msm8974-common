02ec3237a29aa881a3a853bc7cb2917c69176825
Ketut P. Kumajaya <ketut.kumajaya@gmail.com>
2019-01-11 15:01:26 +0000

    msm8974-common: twrp: Use sbin's vold for decryption
    Change-Id: Ib3c29ee105236a88f71a31b5f09095ae6b7393d7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -131,6 +131,7 @@ BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 ifeq ($(WITH_TWRP),true)
 TARGET_RECOVERY_DEVICE_DIRS += $(PLATFORM_PATH)/twrp
 TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_USE_SBIN_VOLD := true
 TW_THEME := portrait_hdpi
 endif
 
