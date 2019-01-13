e0ce469443bb6f01d7df0ad4e969b72d7cf5b726
Michael Bestas <mkbestas@lineageos.org>
2019-01-14 00:49:14 +0200

    msm8974-common: Convert lineagehw to vendor.lineage.touch
    TODO: Implement touchscreen gestures
    
    Change-Id: Id1faae6e8b44b486312e44d204c584cc35a6b677

---
+++ touch/TouchscreenGesture.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "TouchscreenGesture.h"
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace oppo8974 {
+
+// Methods from ::vendor::lineage::touch::V1_0::ITouchscreenGesture follow.
+Return<void> TouchscreenGesture::getSupportedGestures(getSupportedGestures_cb) {
+    // TODO implement
+    return Void();
+}
+
+Return<void> TouchscreenGesture::setGestureEnabled(const ::vendor::lineage::touch::V1_0::Gesture&, bool) {
+    // TODO implement
+    return Void();
+}
+
+}  // namespace oppo8974
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor
