f789f7f62500d349ea8b40fd3b7453749ae33c32
Michael Bestas <mkbestas@lineageos.org>
2020-06-25 11:54:23 -0400

    msm8974-common: Update to power HAL 1.2
    Change-Id: Ic8c5f7e9c77a95befff5661e1634683ec6c25ac4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,8 +115,6 @@ TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Power
-TARGET_HAS_LEGACY_POWER_STATS := true
-TARGET_HAS_NO_WLAN_STATS := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Properties
--- msm8974.mk
+++ msm8974.mk
@@ -199,7 +199,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Ramdisk
 PRODUCT_PACKAGES += \
