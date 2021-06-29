492748b3540ce5e8be3b88142a9fc6390336623e
dianlujitao <dianlujitao@lineageos.org>
2021-06-29 16:42:05 +0200

    msm8974-common: Add prebuilt protobuf from sdk29
    This is needed for our prebuilt DRM library.
    
    Change-Id: I980a9e66cc0f99b5292210428c6ae9737e5b3969

--- msm8974.mk
+++ msm8974.mk
@@ -256,6 +256,10 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
+
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
