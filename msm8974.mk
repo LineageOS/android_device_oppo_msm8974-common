7acd03178cc7795a7f99ecfc0da84d5f38c1dc4e
Robert Shih <robertshih@google.com>
2021-07-15 17:04:54 +0200

    msm8974-common: Enable v1.3 clearkey drm plugin
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: Ie34e59947577988f75fae52d22e3f573f4b92420
    Signed-off-by: Arian <arian.kulmer@web.de>

--- msm8974.mk
+++ msm8974.mk
@@ -83,7 +83,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
