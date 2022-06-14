8e05a52ba70c73b1412fce4e82feb580c1914a8f
Georg Veichtlbauer <georg@vware.at>
2022-06-14 10:41:32 +0200

    msm8974-common: Remove init.recovery.*
    * These don't exist, and they erroneously referred to power.rc
    
    Change-Id: Iddaed31300277317990965ff3fa42f21e1cdc440

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -31,18 +31,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 ifeq ($(TARGET_DEVICE),bacon)
-LOCAL_MODULE       := init.recovery.bacon.rc
-else
-LOCAL_MODULE       := init.recovery.qcom.rc
-endif
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.power.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-ifeq ($(TARGET_DEVICE),bacon)
 LOCAL_MODULE       := ueventd.bacon.rc
 else
 LOCAL_MODULE       := ueventd.qcom.rc
